76369e05c05fb1512ec4dee15cafb3d679e3b2c1
hsnghrld <hassan.ghahraloud@gmail.com>
2021-02-15 17:18:55 +0330

    Change the html file

--- src/main/java/com/github/hsnghrld/Main.java
+++ src/main/java/com/github/hsnghrld/Main.java
@@ -2,29 +2,34 @@ package com.github.hsnghrld;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class Main {
+    private static final String movie_or_series_name = "Lucifer (TV Series)";
+    private static final String movie_or_series_imdb_id = "tt4052886";
+
     public static void main(String[] args) throws IOException {
 
         Document document = Jsoup.parse(
-                new URL("https://maktabkhooneh.org/course/%D8%A2%D9%85%D9%88%D8%B2%D8%B4-Git-mk796/"),
-                10_000
+                new URL(String.format("https://www.imdb.com/title/%s/fullcredits", movie_or_series_imdb_id)),
+                30_000
         );
 
-        document.getElementsByClass("chapter").forEach(element -> {
-            System.out.println(element.getElementsByClass("chapter__title").first().ownText());
-            element.getElementsByClass("chapter__unit").forEach(element1 -> {
-                if (element1.getElementsByClass("chapter__unit-info-item--score").isEmpty())
-                    System.out.printf(
-                            "%c%-40s %s%n",
-                            '\t',
-                            element1.getElementsByClass("h-slider__wrapper").first().ownText(),
-                            element1.getElementsByClass("chapter__unit-time").first().ownText()
-                    );
-            });
+        System.out.printf("Cast of %s%n%n", movie_or_series_name);
+
+        document.getElementsByClass("cast_list").first().getElementsByTag("tr").forEach(element -> {
+            if (!element.hasText())
+                return;
+            System.out.printf(
+                    "%-30s%-30s%s%n",
+                    element.select("td:not([class^='primary_photo']) > a[href*='/name/']").size() > 0 ? element.select("td:not([class^='primary_photo']) > a[href*='/name/']").first().ownText() : "",
+                    element.getElementsByAttributeValueContaining("href", "/characters/").size() > 0 ? element.getElementsByAttributeValueContaining("href", "/characters/").first().ownText() : "",
+                    element.getElementsByClass("toggle-episodes").size() > 0 ? element.getElementsByClass("toggle-episodes").first().ownText() : ""
+            );
         });
 
     }
